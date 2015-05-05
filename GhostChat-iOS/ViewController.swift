7bcb10056087cf929f9630acc2a565603c692ac8
janemcdonough <mcdoj329@newschool.edu>
2015-05-05 16:18:21 -0400

    Changed font/color
    Changed font to Menlo / color scheme to black and green

--- GhostChat-iOS/ViewController.swift
+++ GhostChat-iOS/ViewController.swift
@@ -415,10 +415,10 @@ class ViewController: UIViewController, CBPeripheralManagerDelegate, CBCentralMa
     
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         if section == 0{
-            return "Chat Activity"
+            return ""
         }else if section == 1{
-            tableView.sectionIndexColor = UIColor.darkGrayColor()
-            return "BackGround Devices"
+            tableView.sectionIndexColor = UIColor.blackColor()
+            return ""
         } else {
             return "Misc"
         }
